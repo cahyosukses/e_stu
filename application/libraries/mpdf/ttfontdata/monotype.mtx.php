e303d3bb01b0f3ced149c37245fe8ec9f8114ff4
Herry Satrio <her0satr@yahoo.com>
2015-01-28 11:40:09 +0700

    update

---
+++ application/libraries/mpdf/ttfontdata/monotype.mtx.php
@@ -0,0 +1,26 @@
+<?php
+$name='MonotypeCorsiva';
+$type='TTF';
+$desc=array (
+  'Ascent' => 689,
+  'Descent' => -259,
+  'CapHeight' => 637,
+  'Flags' => 68,
+  'FontBBox' => '[-240 -307 1159 913]',
+  'ItalicAngle' => -14,
+  'StemV' => 87,
+  'MissingWidth' => 750,
+);
+$up=-120;
+$ut=50;
+$ttffile='/home/mohzah9/jafariaschool.org/student/application/libraries/mpdf/ttfonts/MonotypeCorsiva.ttf';
+$TTCfontID='0';
+$originalsize=188188;
+$sip=false;
+$smp=false;
+$BMPselected=false;
+$fontkey='monotype';
+$panose=' a 6 3 1 1 1 1 2 1 1 1 1';
+$haskerninfo=false;
+$unAGlyphs=false;
+?>
